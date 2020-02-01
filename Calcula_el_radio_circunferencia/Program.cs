80c6c4759fd938a9127a23069d9887b3349ad57b
Angel Lopez <enmanuelm15@hotmail.com>
2020-02-01 17:03:38 -0400

    Initial commit

---
+++ Calcula_el_radio_circunferencia/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Calcula_el_radio_circunferencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Este es un programa que calcula la circunferencia de un circulo \n Digite la circunferencia");
+
+            double circunferencia, radio;
+            circunferencia = Convert.ToDouble(Console.ReadLine());
+            radio = circunferencia / (2 * Math.PI);
+
+            Console.WriteLine("El radio de su circulo es: "+ radio);
+
+            Console.ReadKey();
+        }
+    }
+}
